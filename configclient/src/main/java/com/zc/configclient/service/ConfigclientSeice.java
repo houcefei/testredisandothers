412025b209e58d4e2e5ff48b89c9d728c8b508ba
zh <2449936200@qq.com>
2019-08-07 14:54:19 +0800

    aaaa

---
+++ configclient/src/main/java/com/zc/configclient/service/ConfigclientSeice.java
@@ -0,0 +1,11 @@
+package com.zc.configclient.service;
+
+/**
+ * @author hcf
+ * @create 2019-08-07 14:52
+ */
+public class ConfigclientSeice {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
